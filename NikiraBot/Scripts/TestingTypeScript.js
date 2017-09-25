eac8b332cf7ab0eebe234090d3d00373f258b185
mnickhoo <mahdinickhoo@gmail.com>
2017-09-25 11:43:06 -0700

    delete extera class and file in project

--- NikiraBot/Scripts/TestingTypeScript.js
+++
@@ -1,9 +0,0 @@
-//public class person {
-//    name: string;
-//    family: string;
-//    age: string; 
-//}; 
-//person p = new person(){
-//    name : "mahsi"
-//};
-//# sourceMappingURL=TestingTypeScript.js.map
